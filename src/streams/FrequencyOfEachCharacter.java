4eadffc1c2e77fad90108ca0d20ad7e08a4c1c7f
krishanu.goswami <krishanu.goswami@esternibisp.com>
2026-01-29 15:25:33 +0530

    Frequency Of Each Character solution

---
+++ src/streams/FrequencyOfEachCharacter.java
@@ -0,0 +1,27 @@
+package streams;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * Write a Java Stream program to count the occurrence of each character in a string and return a Map<Character, Long>
+ * Input:  "banana"
+ * Output: {b=1, a=3, n=2}
+ */
+public class FrequencyOfEachCharacter {
+    public static void main(String[] args) {
+        String input = "banana";
+
+        Map<Character, Long> result = input.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        LinkedHashMap::new,
+                        Collectors.counting()
+                ));
+
+        System.out.println(result);
+    }
+}
