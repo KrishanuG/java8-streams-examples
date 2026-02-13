00df51dc372bab237ba38943fdb832ae2dd54af5
krishanu.goswami <krishanu.goswami@esternibisp.com>
2026-02-13 18:08:35 +0530

    remove unused import

--- src/streams/hard/ReverseEachWordInSentence.java
+++ src/streams/hard/ReverseEachWordInSentence.java
@@ -2,7 +2,6 @@ package streams.hard;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.stream.Collectors;
 
 public class ReverseEachWordInSentence {
